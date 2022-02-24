ad512d3c7020398b6e68e5d8a2120aef0bb64898
xuan1083355 <100353401+xuan1083355@users.noreply.github.com>
2022-02-24 20:30:51 +0800

    Week1_exercise

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+    <?php
+    $i=10;
+    $j=20;
+    echo "<b>Hello</b> <h1>PHP!</h1>";
+    echo "<br/>";
+    echo $i+$j;
+    echo "<br/>";
+    echo date("Y,m,d");
+    ?>
+</body>
+</html>
